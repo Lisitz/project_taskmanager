0d6d6b95a60b50c67c331938065f144a1c806230
Daria <krasniy.lisitz@gmail.com>
2021-07-06 15:06:44 +0300

    init

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-$m4h7t%qfusd6k!^$luihv_u1*51gw$26^_f+thb6r)20gqbhq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
